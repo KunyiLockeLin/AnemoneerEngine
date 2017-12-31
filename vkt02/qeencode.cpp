757ac10e51df93a772e677d63a682d436d3af0b7
林昆毅 Kunyi Locke Lin <snowcrow0802@gmail.com>
2017-12-31 00:32:27 -0600

    decode jpeg

--- vkt02/qeencode.cpp
+++ vkt02/qeencode.cpp
@@ -275,7 +275,14 @@ QeAssetModel* QeEncode::decodeOBJ(char* buffer) {
 	return model;
 }
 
-QeAssetModel* QeEncode::decodeGLTF(char* buffer) { return nullptr; }
+QeAssetModel* QeEncode::decodeGLTF(char* buffer) { 
+
+	int index = 0;
+	QeAssetJSON *json = decodeJSON(buffer, index);
+
+	return nullptr; 
+}
+
 QeAssetModel* QeEncode::decodeGLB(char* buffer) { return nullptr; }
 
 QeAssetMaterial* QeEncode::decodeMTL(char* buffer) {
@@ -343,6 +350,115 @@ QeAssetMaterial* QeEncode::decodeMTL(char* buffer) {
 
 std::vector<unsigned char> QeEncode::decodeJPEG(unsigned char* buffer, int* width, int* height, int* bytes) {
 	std::vector<unsigned char> ret;
+	// YCbCr(YUV), DCT(Discrete Cosine Transform), Quantization, Z(Entropy Coding), RLC(Run Length Coding), Canonical Huffman Code
+	unsigned char startKey[2]	= { 0xFF, 0xD8 }; // SOI
+	unsigned char APP0[2]		= { 0xFF, 0xE0 };
+	unsigned char quanKey[2]	= { 0xFF, 0xDB }; // DQT
+	unsigned char inforKey[2]	= { 0xFF, 0xC0 }; // SOF
+	unsigned char huffmanKey[2] = { 0xFF, 0xC4 }; // DHT
+	unsigned char scanKey[2]	= { 0xFF, 0xDA }; // SOS
+	unsigned char endKey[2]		= { 0xFF, 0xD9 }; // EOI
+
+	if (memcmp(buffer, startKey, 2) != 0) return ret;
+
+	unsigned short int length = 0;
+	char* key;
+	char* data = nullptr;
+	size_t index = 2;
+	unsigned char num[2];
+	unsigned char* dataPos = nullptr;
+	char colorBits;
+	char colorNum;
+	QeHuffmanTree2 DC0, AC0, DC1, AC1;
+	int i = 0;
+
+	while (1) {
+
+		key = (char*)(buffer + index);
+		num[0] = *(buffer + index + 3);
+		num[1] = *(buffer + index + 2);
+		length = *(unsigned short int*)(num)-2;
+		data = (char*)(buffer + index + 4);
+
+		if (memcmp(key, APP0, 2) == 0) {
+			int a = 0;
+		}
+		else if (memcmp(key, quanKey, 2) == 0) {
+			int a = 0;
+		}
+		else if (memcmp(key, inforKey, 2) == 0) {
+			colorBits = data[0];
+			num[1] = data[1];
+			num[0] = data[2];
+			*width = *(short int*)&num;
+			num[1] = data[3];
+			num[0] = data[4];
+			*height = *(short int*)&num;
+			colorNum = data[5];
+
+			char bits = 0;
+			if (colorNum == 1)	bits = colorBits;
+			else				bits = colorBits * 3;
+			*bytes = (bits + 7) / 8;
+		}
+		else if (memcmp(key, huffmanKey, 2) == 0) {
+			
+			QeHuffmanTree2 *tree = nullptr;
+			if (data[0] == 0x00) tree = &DC0;
+			else if (data[0] == 0x10) tree = &AC0;
+			else if (data[0] == 0x01) tree = &DC1;
+			else if (data[0] == 0x11) tree = &AC1;
+
+			unsigned char treeIndex[16];
+			size_t length = 0;
+			for (i = 0;i<16;++i) {
+				treeIndex[i] = data[1+i];
+				length += treeIndex[i];
+			}
+
+			tree->values= new char[length];
+			tree->codes = new unsigned short int[length];
+			tree->codes[0] = 0;
+
+			int itreeIndex = 0;
+			int ntreeIndex = 1;
+			while (treeIndex[itreeIndex] ==0 ) ++itreeIndex;
+			int incresingLength = 0;
+
+			i = 0;
+			while(1){
+				tree->values[i] = data[17 + i];
+
+				if (i == length) break;
+
+				while (treeIndex[itreeIndex]<=ntreeIndex) {
+					++itreeIndex;
+					++incresingLength;
+					ntreeIndex = 0;
+				}
+				tree->codes[i + 1]= tree->codes[i]+1;
+
+				if (ntreeIndex == 0) {
+					tree->codes[i + 1] <<= incresingLength;
+					incresingLength = 0;
+				}
+				++ntreeIndex;
+				++i;
+			}
+		}
+		else if (memcmp(key, scanKey, 2) == 0) {
+
+			index += (length + 2 * 2);
+			dataPos = buffer + index;
+			break;
+		}
+		index += (length + 2*2);
+	}
+
+	/*while (1) {
+	
+		if (memcmp(dataPos, endKey, 2) == 0) break;
+	}*/
 	return ret;
 }
 
@@ -352,10 +468,10 @@ std::vector<unsigned char> QeEncode::decodeBMP(unsigned char* buffer, int* width
 	if (strncmp((char*)buffer, "BM", 2) != 0) return ret;
 
 
-	*width = *(int*)&(buffer[0x12]);
-	*height = *(int*)&(buffer[0x16]);
-	short int bits = *(short int*)&(buffer[0x1C]);
-	*bytes = (bits + 7) / 8;
+	*width = *(int*)(buffer+0x12);
+	*height = *(int*)(buffer+0x16);
+	short int *bits = (short int*)(buffer+0x1C);
+	*bytes = (*bits + 7) / 8;
 	int	imageSize = *width * *height * *bytes;
 
 	ret.resize(imageSize);
@@ -379,12 +495,12 @@ std::vector<unsigned char> QeEncode::decodePNG(unsigned char* buffer, int* width
 	num[1] = buffer[0x12];
 	num[2] = buffer[0x11];
 	num[3] = buffer[0x10];
-	*width = *(int*)&(num);
+	*width = *(int*)&num;
 	num[0] = buffer[0x17];
 	num[1] = buffer[0x16];
 	num[2] = buffer[0x15];
 	num[3] = buffer[0x14];
-	*height = *(int*)&(num);
+	*height = *(int*)&num;
 	int bitDepth = buffer[0x18];
 	int colorType = buffer[0x19];
 	int bits = 0;
@@ -508,7 +624,7 @@ std::vector<unsigned char> QeEncode::decodePNG(unsigned char* buffer, int* width
 }
 
 std::vector<unsigned char> QeEncode::decodeDeflate(unsigned char* in, unsigned int size) {
-	// gzip zlib Deflate LZ77 Huffman inflate
+	// gzip, zlib, Deflate, LZ77, Canonical Huffman Code, inflate
 	// 78 01 - fastest
 	// 78 5E - fast 
 	// 78 9C - default
@@ -549,13 +665,13 @@ void QeEncode::decodeHuffmanLZ77(std::vector<unsigned char> *out, unsigned char*
 	// literal/length, distance for LZ77
 	QeHuffmanTree treeLL, treeD;
 	// decodeCCL
-	if (BYTE == 1)		buildFixedHuffmanTree(&treeLL, &treeD);							// fixed Huffman 
-	else if (BYTE == 2) buildDynamicHuffmanTree(&treeLL, &treeD, in, bitPointer);	// dynamic Huffman 
+	if (BYTE == 1)		buildFixedLZ77HuffmanTree(&treeLL, &treeD);							// fixed Huffman 
+	else if (BYTE == 2) buildDynamicLZ77HuffmanTree(&treeLL, &treeD, in, bitPointer);	// dynamic Huffman 
 
 	return decodeLitLenDis(out, &treeLL, &treeD, in, bitPointer);
 }
 
-void QeEncode::buildFixedHuffmanTree(QeHuffmanTree* treeLL, QeHuffmanTree* treeD) {
+void QeEncode::buildFixedLZ77HuffmanTree(QeHuffmanTree* treeLL, QeHuffmanTree* treeD) {
 	// 0-255=literals(ASCII), 256=end, 257-285=lengths, 286-287=unused
 	unsigned int bitlenLL[288];
 	unsigned int i = 0;
@@ -571,7 +687,7 @@ void QeEncode::buildFixedHuffmanTree(QeHuffmanTree* treeLL, QeHuffmanTree* treeD
 	buildHuffmanTree(treeD, bitlenD, 288, 15);
 }
 
-void QeEncode::buildDynamicHuffmanTree(QeHuffmanTree* treeLL, QeHuffmanTree* treeD, const unsigned char* in, size_t* bitPointer){
+void QeEncode::buildDynamicLZ77HuffmanTree(QeHuffmanTree* treeLL, QeHuffmanTree* treeD, const unsigned char* in, size_t* bitPointer){
 
 	unsigned int HLIT = readBits(in, bitPointer, 5) + 257;
 	unsigned int HDIST = readBits( in, bitPointer, 5) + 1;
