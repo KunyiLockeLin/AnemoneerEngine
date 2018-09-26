3fe374e4a678d3c41eb50e91866e5e06da1d5a82
林昆毅 Kunyi Locke Lin <snowcrow0802@gmail.com>
2018-09-26 10:16:39 -0500

    ui

--- vkt02/main.cpp
+++ vkt02/main.cpp
@@ -3,14 +3,12 @@
 
 int main() {
 	
-	while (1) {
-		try {
-			QE->run();
-		}
-		catch (const std::runtime_error& e) {
-			LOG( e.what() );
-			return EXIT_FAILURE;
-		}
+	try {
+		QE->run();
+	}
+	catch (const std::runtime_error& e) {
+		LOG( e.what() );
+		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 }
