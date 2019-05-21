c92f7e88a3b85b8046151051eb2b848cadf2cf77
林昆毅 Kunyi(Locke) Lin <kunyilockelin@gmail.com>
2019-05-20 20:23:31 -0600

    Merge main.cpp into engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -44,3 +44,13 @@ void QueenEngine::mainLoop() {
     }
     vkDeviceWaitIdle(VK->device);
 }
+
+int main() {
+    try {
+        QE->run();
+    } catch (const std::runtime_error &e) {
+        LOG(e.what());
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
