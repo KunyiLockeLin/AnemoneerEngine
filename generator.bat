7bdaa2d33bda61caad3f9c0974a23017a8b35ce8
kunyilockelin <kunyilockelin@gmail.com>
2020-05-12 01:53:45 -0600

    Update generator.bat

--- generator.bat
+++ generator.bat
@@ -1,24 +1,28 @@
+@echo | call output/data/shader_compile.bat
+timeout 3
+
 rmdir build /s/q
 mkdir build
 cd build
 cmake -G "Visual Studio 15 Win64" ..
+
 cmake --build . --target CodeGenerator --config Debug
 timeout 3
+
 cd ..
 @echo | call code_generator.bat
 timeout 3
+
 cd build
 cmake --build . --target AngryEngine --config Debug
 timeout 3
-start AngryEngine.sln
 
-cd..
-@echo | call output/data/shader_compile.bat
-timeout 3
-
+cd ..
 cd output
 start AngryEngine_debug.exe
 timeout 10
 taskkill /F /im AngryEngine_debug.exe
+
+start AngryEngine.sln
 
 pause
