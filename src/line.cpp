26c9e5cde3e3cd212b918061b8ee032c52823c96
kunyilockelin <kunyilockelin@gmail.com>
2020-05-16 20:19:23 -0600

    aaa

--- src/line.cpp
+++ src/line.cpp
@@ -1,7 +1,7 @@
 #include "header.h"
 
 void QeLine::initialize(AeXMLNode *_property, QeObject *_owner) {
-    COMPONENT_INITIALIZE_PARENT(QeModel)
+    COMPONENT_INITIALIZE
 
     VK->createBuffer(modelBuffer, sizeof(bufferData), nullptr);
 
